69a073420ffdf28af9bcaba6d3a43e13c94c00fb
kvasovat <tereza.kvasova2@gmail.com>
2022-06-30 21:48:58 +0200

    Prepare keyboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'keyboard';
+
+  buttons = [['a', 'b', 'c', 'd'], ['e', 'f', 'g', 'h'], ['i', 'j', 'k', 'l'], ['m', 'n', 'o', 'p'], ['q', 'r', 's', 't'], ['u', 'v', 'w', 'x'], ['y', 'z'], ['_', '.', '?', '!'], ['←', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',]]
+  section = 0;
+  selectedCharIndex = undefined;
+  time = 0;
+
+  text = ''
 }
